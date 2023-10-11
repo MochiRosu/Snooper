40b4eb0c968737f1cdaeee7d26fe6c313d114133
Roscoe Collard <129026799+MochiRosu@users.noreply.github.com>
2023-10-12 07:47:39 +1100

    made my bullet able to travel straight

--- Snooper/Assets/Scripts/Gun.cs
+++ Snooper/Assets/Scripts/Gun.cs
@@ -1,4 +1,3 @@
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
@@ -12,6 +11,8 @@ public class Gun : MonoBehaviour
     public int numberOfPoints;
     public float spaceBetweenPoints;
     Vector2 Direction;
+
+    private bool isAiming; // Indicates whether the player is currently aiming
 
     private void Start()
     {
@@ -27,18 +28,38 @@ public class Gun : MonoBehaviour
 
     private void Update()
     {
-        Vector2 gunPosition = transform.position;
-        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Direction = mousePosition - gunPosition;
-        transform.right = Direction;
+        // Detect a tap and hold
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
 
-        if(Input .GetMouseButtonDown(0))
-        {
-            Shoot();
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    isAiming = true;
+                    break;
+
+                case TouchPhase.Ended:
+                    isAiming = false;
+                    Shoot(); // Fire the bullet when the finger is lifted
+                    break;
+            }
+
+            if (isAiming)
+            {
+                Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+                Direction = touchPosition - (Vector2)transform.position;
+                transform.right = Direction;
+            }
         }
-        for (int i = 0; i <numberOfPoints;i++)
+
+        // Update trajectory prediction points while aiming
+        if (isAiming)
         {
-            points[i].transform.position = PointPosition(i * spaceBetweenPoints);
+            for (int i = 0; i < numberOfPoints; i++)
+            {
+                points[i].transform.position = PointPosition(i * spaceBetweenPoints);
+            }
         }
     }
 
@@ -46,11 +67,12 @@ public class Gun : MonoBehaviour
     {
         // Create a bullet and set its position and force
         GameObject newBullet = Instantiate(bullet, shotPoint.position, shotPoint.rotation);
-        newBullet.GetComponent<Rigidbody2D>().velocity = transform.right * launchForce ;
+        newBullet.GetComponent<Rigidbody2D>().velocity = transform.right * launchForce;
     }
+
     Vector2 PointPosition(float t)
     {
-        Vector2 position = (Vector2)shotPoint.position + (Direction.normalized * launchForce * t) + 0.5f * Physics2D.gravity * (t * t);
+        Vector2 position = (Vector2)shotPoint.position + (Direction.normalized * launchForce * t);
         return position;
     }
 }
