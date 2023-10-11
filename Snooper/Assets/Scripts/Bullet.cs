d39a535c80e4bf27cefc89d176070e3153be3df3
Roscoe Collard <129026799+MochiRosu@users.noreply.github.com>
2023-10-11 15:45:15 +1100

    Started fresh

--- Snooper/Assets/Scripts/Bullet.cs
+++ Snooper/Assets/Scripts/Bullet.cs
@@ -4,7 +4,6 @@ public class Bullet : MonoBehaviour
 {
     Rigidbody2D rb;
     bool hasHit;
-    
 
     void Start()
     {
@@ -22,8 +21,6 @@ public class Bullet : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        hasHit = true;
-        rb.velocity = Vector2.zero;
-        rb.isKinematic = true;
+       
     }
 }
