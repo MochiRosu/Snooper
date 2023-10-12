67a538c640c7863465f2e39d4a5830ceae214f76
Roscoe Collard <129026799+MochiRosu@users.noreply.github.com>
2023-10-13 05:40:32 +1100

    Added main menu that goes to buttons

---
+++ Snooper/Assets/Scripts/MainMenuUI.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public string level1SceneName = "Level1"; // The name of your first level scene
+    public string level2SceneName = "Level2"; // The name of your second level scene
+    public string level3SceneName = "Level3"; // The name of your third level scene
+
+    // Add public functions to be called from UI buttons
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene(level1SceneName);
+    }
+
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene(level2SceneName);
+    }
+
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene(level3SceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
